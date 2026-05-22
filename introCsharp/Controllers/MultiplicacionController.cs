19b23970a8565913e76d99e70dad72c59c222f19
T <papilozanowest@gmail.com>
2026-05-21 19:23:05 -0600

    Multiplicacion 21/05/2026

--- introCsharp/Controllers/MultiplicacionController.cs
+++ introCsharp/Controllers/MultiplicacionController.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using introCsharp.Models;
+using System.Web.Mvc;
+
+namespace introCsharp.Controllers
+{
+    public class MultiplicacionController : Controller
+    {
+        public ActionResult Multiplicacion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Multiplicacion(Multiplicacion mul)
+        {
+            mul.Multiplicar();
+
+            return View(mul);
+        }
+    }
+}
--- introCsharp/Models/Multiplicacion.cs
+++ introCsharp/Models/Multiplicacion.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace introCsharp.Models
+{
+    public class Multiplicacion
+    {
+        public int A { get; set; }
+
+        public int B { get; set; }
+
+        public int Res { get; set; }
+
+        public string Operacion { get; set; }
+
+        public void Multiplicar()
+        {
+            int resultado = 0;
+
+            string suma = "";
+
+            for (int i = 0; i < B; i++)
+            {
+                resultado = resultado + A;
+
+                suma += A.ToString();
+
+                if (i < B - 1)
+                {
+                    suma += "+";
+                }
+            }
+
+            suma += "=" + resultado;
+
+            this.Res = resultado;
+
+            this.Operacion = suma;
+        }
+    }
+}
